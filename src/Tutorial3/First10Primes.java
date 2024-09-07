8565bc8b2a19a8c9b3c33cf6f49caf74bcaf90c5
daichit <gene.sixteen.000925@outlook.jp>
2024-09-07 14:22:37 +0700

    [Fix] more code

--- src/Tutorial3/First10Primes.java
+++ src/Tutorial3/First10Primes.java
@@ -1,21 +1,6 @@
 package Tutorial3;
 import java.util.*;
 public class First10Primes {
-    public static void main(String[] args) {
-        List<Integer> array = new ArrayList<>();
-        int num =1 ,count = 0;
-        while (count < 10) {
-            if (isPrimeNumber(num)) {
-                array.add(num);
-                count++;
-            }
-            num++;
-        }
-
-        for (Integer integer : array) {
-            System.out.println(integer);
-        }
-    }
 
     private static boolean isPrimeNumber(int num) {
         if (num <= 1) {
@@ -30,7 +15,37 @@ public class First10Primes {
         }
         return flag;
     }
+
+    public static void main(String[] args) {
+        List<Integer> array = new ArrayList<>();
+        int num =1 ,count = 0;
+        while (count < 10) {
+            if (isPrimeNumber(num)) {
+                array.add(num);
+                count++;
+            }
+            num++;
+        }
+        Iterator<Integer> arr = array.iterator();
+//        int countIre = 0;
+        while (arr.hasNext()){
+            System.out.println(arr.next());
+//            countIre ++;
+//            System.out.println(countIre + " times iteration");
+//            print value == more larger than  5
+//            int element = arr.next();
+            // error https://github.com/daichitt/SE1-FIT/tree/main
+//            if (arr.next() > 5) {
+//                System.out.println(arr.next());
+//            }
+        }
+        System.out.println("-----------");
+//        for (int nu : array) {
+//            System.out.println(nu);
+//        }
+//        System.out.println("List Elements : " + array.size());
+    }
+
+
 }
-
-
 // only do 1 4 5
