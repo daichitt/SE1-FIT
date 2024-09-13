ff005352316718ee6507d4d541a7b81eb61600ed
daichit <gene.sixteen.000925@outlook.jp>
2024-09-13 12:53:57 +0700

    add LifecycleJUnit5Test

---
+++ src/Tutorial4/LifecycleJUnit5Test.java
@@ -0,0 +1,39 @@
+package Tutorial4;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LifecycleJUnit5Test {
+    @BeforeAll
+    static void setupAll() {
+        System.out.println("Setup ALL TESTS in the class");
+    }
+
+    @BeforeEach
+    void setup() {
+        System.out.println("Setup EACH TEST in the class");
+    }
+
+    @Test
+    void testOne() {
+        System.out.println("TEST 1");
+    }
+
+    @Test
+    void testTwo() {
+        System.out.println("TEST 2");
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("Teardown EACH TEST in the class");
+    }
+
+    @AfterAll
+    static void teardownAll() {
+        System.out.println("Teardown ALL TESTS in the class");
+    }
+}
